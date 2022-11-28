21dddd0c6ff54313f164f0ff689a9b734df1877a
RDJinTropicThunder <nchristian02@student.ciputra.ac.id>
2022-11-28 11:49:47 +0700

    add basic challenge controller

---
+++ controllers/challengeController.go
@@ -0,0 +1,68 @@
+package controllers
+
+import (
+	"github.com/NCInside/procod_01/initializers"
+	"github.com/NCInside/procod_01/models"
+	"github.com/gin-gonic/gin"
+)
+
+var bodyChal struct {
+	Title       string
+	Description string
+	UserID      uint
+}
+
+func ChallengeCreate(c *gin.Context) {
+	c.Bind(&bodyChal)
+
+	challenge := models.Challenge{Title: bodyChal.Title, Description: bodyChal.Description, UserID: bodyChal.UserID}
+	result := initializers.DB.Create(&challenge)
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	c.JSON(200, challenge)
+}
+
+func ChallengeIndex(c *gin.Context) {
+	var challenges []models.Challenge
+	initializers.DB.Find(&challenges)
+
+	c.JSON(200, challenges)
+}
+
+func ChallengeShow(c *gin.Context) {
+	id := c.Param("id")
+
+	var challenge models.Challenge
+	initializers.DB.First(&challenge, id)
+
+	c.JSON(200, challenge)
+}
+
+func ChallengeUpdate(c *gin.Context) {
+	id := c.Param("id")
+
+	c.Bind(&bodyChal)
+
+	var challenge models.Challenge
+	initializers.DB.First(&challenge, id)
+
+	initializers.DB.Model(&challenge).Updates(models.Challenge{
+		Title:       bodyChal.Title,
+		Description: bodyChal.Description,
+		UserID:      bodyChal.UserID,
+	})
+
+	c.JSON(200, challenge)
+}
+
+func ChallengeDelete(c *gin.Context) {
+	id := c.Param("id")
+
+	initializers.DB.Delete(&models.Challenge{}, id)
+
+	c.Status(200)
+}
--- main.go
+++ main.go
@@ -14,18 +14,27 @@ func init() {
 func main() {
 	router := gin.Default()
 
+	//User
 	router.POST("/users", controllers.UserCreate)
 	router.PUT("/users/:id", controllers.UserUpdate)
 	router.GET("/users", controllers.UserIndex)
 	router.GET("/users/:id", controllers.UserShow)
 	router.DELETE("/users/:id", controllers.UserDelete)
 
+	//Statistic
 	router.POST("/statistics", controllers.StatisticCreate)
 	router.PUT("/statistics/:id", controllers.StatisticUpdate)
 	router.GET("/statistics", controllers.StatisticIndex)
 	router.GET("/statistics/:id", controllers.StatisticShow)
 	router.DELETE("/statistics/:id", controllers.StatisticDelete)
 	router.GET("/statistics/:userid", controllers.StatisticIndexUser)
+
+	//Challenge
+	router.POST("/challenge", controllers.ChallengeCreate)
+	router.PUT("/challenge/:id", controllers.ChallengeUpdate)
+	router.GET("/challenge", controllers.ChallengeIndex)
+	router.GET("/challenge/:id", controllers.ChallengeShow)
+	router.DELETE("/challenge/:id", controllers.ChallengeDelete)
 
 	router.Run()
 }
